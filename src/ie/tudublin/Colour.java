468653f80e042aba4a8a88d82cd5359cda28aed5
Mariana Pirtac <c17378303@mydit.ie>
2019-04-10 16:39:18 +0100

    Created the Constructor and toString function

--- src/ie/tudublin/Colour.java
+++ src/ie/tudublin/Colour.java
@@ -7,4 +7,16 @@ public class Colour
    public float g;
    public float b;
 
+   public Colour(String colour, float r, float g, float b)
+   {
+       this.colour = colour;
+       this.r = r;
+       this.g = g;
+       this.b = b;
+   }
+
+   public String toString()
+   {
+       return colour + "/t" + r + "/t" + g + "/t" + b;
+    }
 }
